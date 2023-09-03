d18896a2ae6af7a184bb26d746d63df701cee65a
RelativisticMechanic <cyclicintegral@gmail.com>
2023-09-04 00:35:23 +0530

    sync 1

---
+++ resources/js/chapter.js
@@ -0,0 +1,62 @@
+function AddSidebar() {
+
+    var number_of_verses = $('#chapter_information').data('n-verses');
+    var chapter_no = $('#chapter_information').data('chapter-no');
+
+    var offcanvas_html = `
+    <img src="/resources/images/sidebar.png" class="sidebar-button" data-bs-toggle="offcanvas" data-bs-target="#verse_browser_offcanvas" aria-controls="verse_browser_offcanvas"></button>
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="verse_browser_offcanvas" aria-labelledby="verse_browser_offcanvas_label">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="verse_browser_offcanvas_label">${chapter_no}. ${chapter_titles_english[chapter_no]}</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body" id="verse_browser_div">
+    </div>
+    </div>
+    `;
+
+    var content = `<ul class="list-group" id="verse_browser">`
+    for (var i = 1; i <= number_of_verses; i++) {
+        content += `<a href="#${i}"><li class="list-group-item">${chapter_no}.${i}</li></a>`;
+    }
+    content += "</ul>";
+    $('body').append(offcanvas_html);
+    $("#verse_browser_div").append(content);
+}
+
+function ProgressBarScrollCallback() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop,
+        height = document.documentElement.scrollHeight - document.documentElement.clientHeight,
+        scrolled = (winScroll / height);
+
+    /* Since we have disabled scroll bars, we will display the scroll position as a line on the top of the page. */
+    if (scrolled <= 0) {
+        $(".progress-container").fadeOut();
+    }
+    else {
+        $(".progress-container").fadeIn();
+        document.getElementById("progressBar").style.width = (scrolled * 100) + "%";
+    }
+}
+
+function ShareVerseLink(modal, selector, chapter, verse)
+{
+    var domain = location.protocol + '//' + location.host;
+    var verseURL = `${domain}/${chapter}.html#${verse}`;
+    $(selector).val(verseURL);
+    $(modal).modal("show");
+}
+
+function CopyVerseLinktoClipboard(selector)
+{
+    copyTextToClipboard($(selector).val());
+}
+
+$(document).ready(() => {
+    /* Hide the progress bar */
+    $(".progress-container").hide();
+    document.addEventListener('scroll', () => {
+        ProgressBarScrollCallback();
+    });
+    AddSidebar();
+})
---
+++ resources/js/titles.js
@@ -0,0 +1,41 @@
+var chapter_titles = {
+    1: "अर्जुनविषादयोगः",
+    2: "साङ्ख्ययोगः",
+    3: "कर्मयोगः",
+    4: "ज्ञानकर्मसंन्यासयोगः",
+    5: "संन्यासयोगः",
+    6: "आत्मसंयमयोगः",
+    7: "ज्ञानविज्ञानयोगः",
+    8: "अक्षरब्रह्मयोगः",
+    9: "राजविद्याराजगुह्ययोगः",
+    10: "विभूतियोगः",
+    11: "विश्वरूपदर्शनयोगः",
+    12: "भक्तियोगः",
+    13: "क्षेत्रक्षेत्रज्ञविभागयोगः",
+    14: "गुणत्रयविभागयोगः",
+    15: "पुरुषोत्तमयोगः",
+    16: "दैवासुरसम्पद्विभागयोगः",
+    17: "श्रद्धात्रयविभागयोगः",
+    18: "मोक्षसंन्यासयोगः"
+}
+
+var chapter_titles_english = {
+    1: "Lamenting the Consequences of War",
+    2: "The Way of Knowledge",
+    3: "Action",
+    4: "Action versus Inaction",
+    5: "Renunciation",
+    6: "Yoga",
+    7: "Realisation of the Supreme",
+    8: "The Way to the Supreme",
+    9: "The Kingly Secret",
+    10: "Glimpse of the Supreme",
+    11: "The Cosmic Form of God",
+    12: "Devotion",
+    13: "The Cosmic Field",
+    14: "Three Qualities of Nature",
+    15: "The Supreme Spirit",
+    16: "The Fall of Man",
+    17: "Three Kinds of Men",
+    18: "Liberation"
+}
